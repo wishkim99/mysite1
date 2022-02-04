24d91f9ca5e10696794e507d9c50d441780286d0
wishkim99 <zzsowon@naver.com>
2022-02-04 09:28:20 +0900

    AuthInterceptor 강사님 코드 추가설명

--- mysite03/src/main/java/com/poscoict/mysite/security/AuthInterceptor.java
+++ mysite03/src/main/java/com/poscoict/mysite/security/AuthInterceptor.java
@@ -29,15 +29,18 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 		if(auth==null) {
 			//한줄이면 됨
 			auth=handlerMethod.getBeanType().getAnnotation(Auth.class);
-			//Auth라는 클래스이름으로 해당 bean타입을 얻어오겠다/
+			//Auth라는 클래스이름으로 해당 bean타입을 얻어오겠다
+			
+			/*강사님 코드*/
+//			handlerMethod.getMethod().getDeclaringClass().getAnnotation(Auth.class);
 		}
 
-		//5. type과 method에 @Auth가 적용이 안되어있는 경우
+		//5. type(4번)과 method(3번)에 @Auth가 적용이 안되어있는 경우
 		if(auth == null) {
 			return true; //true면 그냥 진행됨
 		}
 		
-		//5. @Auth가 적용이 되어있기 때문에 인증(Authentication)여부 확인
+		//6. @Auth가 적용이 되어있기 때문에 인증(Authentication)여부 확인
 		HttpSession session=request.getSession();
 		if(session==null) {
 			//session이 없으니까 loginform으로 다시 요청
@@ -52,6 +55,26 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 			return false;
 		}
 		
+		/*강사님 코드
+		//7.권한(Authorization) 체크를 위해서 @Auth의 role 가져오기("USER","ADMIN")
+		String role=auth.role();
+		
+		//8. @Auth의 role이 "USER"인 경우, authUser의 role은 상관이 없다.
+		if("User".equals(role)) {
+			return true;
+		}
+		
+		//9. @Auth의 role이 "ADMIN"인 경우, authUser은 "ADMIN"이어야 한다.
+		if("ADMIN".equals(authUser.getRole())==false) {
+			response.sendRedirect(request.getContextPath());
+			return false;
+		}
+		
+		//10. 옳은 관리자
+		//@Auth의 role: "ADMIN"
+		//authUser의 role:"ADMIN"
+		 return true;
+		*/
 		if(authUser.getRole().equals("ADMIN")) {//null이면 인증이 안되어있다는 의미
 			return true;
 		}
